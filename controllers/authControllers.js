4d6f17bf379f9fc0dca70b865d4c42135cb0aee2
hetx19 <hetsavaliya19@gmail.com>
2025-06-27 12:49:14 +0530

    Update User Api

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const cloudinary = require("../config/cloudinary");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "2h" });
@@ -86,8 +87,111 @@ const getUser = async (req, res) => {
   }
 };
 
+const uploadImage = (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No File Uploaded" });
+    }
+
+    cloudinary.uploader.upload(
+      req.file.path,
+      { folder: "expense-tracker" },
+      (err, result) => {
+        if (err) {
+          console.error(err);
+        } else {
+          return res.status(200).json({ imageUrl: result.secure_url });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { name, email, password, profileImageUrl } = req.body;
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+
+    user.name = name || user.name;
+    user.profileImageUrl = profileImageUrl || user.profileImageUrl;
+
+    if (email) {
+      const checkEmail = await User.findOne({ email });
+      if (checkEmail) {
+        return res
+          .status(400)
+          .json({ message: "User With This Email Already Exists" });
+      } else {
+        user.email = email;
+      }
+    }
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      message: "User Updated Successfully",
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      profileImageUrl: updatedUser.profileImageUrl,
+      token: generateToken(updatedUser._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+const updateImage = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!req.file) {
+      return res.status(200).json({ imageUrl: user.profileImageUrl });
+    }
+
+    const array = user.profileImageUrl.split("/");
+    const image = array[array.length - 1];
+    const imageName = image.split(".")[0];
+
+    if (req.file) {
+      cloudinary.api.delete_resources([`expense-tracker/${imageName}`], {
+        type: "upload",
+        resource_type: "image",
+      });
+
+      cloudinary.uploader.upload(
+        req.file.path,
+        { folder: "expense-tracker" },
+        (err, result) => {
+          if (err) {
+            console.error(err);
+          } else {
+            return res.status(200).json({ imageUrl: result.secure_url });
+          }
+        }
+      );
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
 module.exports = {
   signUpUser,
   signInUser,
   getUser,
+  updateUser,
+  uploadImage,
+  updateImage,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const protect = require("../middleware/auth");
-const cloudinary = require("../config/cloudinary");
 const upload = require("../middleware/upload");
 const {
   signUpUser,
   signInUser,
   getUser,
+  updateUser,
+  uploadImage,
+  updateImage,
 } = require("../controllers/authControllers");
 
 const router = express.Router();
@@ -13,23 +15,10 @@ const router = express.Router();
 router.post("/signup", signUpUser);
 router.post("/signin", signInUser);
 router.get("/getUser", protect, getUser);
+router.put("/updateUser", protect, updateUser);
 
-router.post("/upload-image", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: "No File Uploaded" });
-  }
+router.post("/upload-image", upload.single("image"), uploadImage);
 
-  cloudinary.uploader.upload(
-    req.file.path,
-    { folder: "expense-tracker" },
-    (err, result) => {
-      if (err) {
-        console.error(err);
-      } else {
-        return res.status(200).json({ imageUrl: result.secure_url });
-      }
-    }
-  );
-});
+router.put("/update-image", protect, upload.single("image"), updateImage);
 
 module.exports = router;
