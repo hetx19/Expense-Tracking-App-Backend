809cf4aea4412268bcc07f8101587ba822ff417e
Harshal Savaliya <hetsavaliya19@gmail.com>
2026-08-14 19:24:09 +0530

    refactor: intoduce service/repository layers for expense resource

--- controllers/expenseControllers.js
+++ controllers/expenseControllers.js
@@ -1,42 +1,24 @@
-const xlsx = require("xlsx");
-const Expense = require("../models/Expense");
-const AppError = require("../utils/AppError");
+const expenseService = require("../services/expense.service");
 const asyncHandler = require("../utils/asyncHandler");
 
 const addExpense = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-  const { icon, category, amount, date } = req.body;
-
-  const newExpense = new Expense({
-    userId,
-    icon,
-    category,
-    amount,
-    date: new Date(date),
+  const newExpense = await expenseService.addExpense({
+    userId: req.user._id,
+    ...req.body,
   });
-
-  await newExpense.save();
-
   res.status(200).json(newExpense);
 });
 
 const getAllExpense = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-  const expense = await Expense.find({ userId }).sort({ date: -1 });
-
+  const expense = await expenseService.getAllExpenses(req.user._id);
   res.json(expense);
 });
 
 const deleteExpense = asyncHandler(async (req, res) => {
-  const deletedExpense = await Expense.findOneAndDelete({
-    _id: req.params.id,
-    userId: req.user._id,
-  });
-
-  if (!deletedExpense) {
-    throw new AppError("Expense Not Found", 404);
-  }
-
+  const deletedExpense = await expenseService.deleteExpense(
+    req.params.id,
+    req.user._id,
+  );
   res.json({
     message: "Expense Deleted Successfully",
     deletedExpense,
@@ -44,19 +26,7 @@ const deleteExpense = asyncHandler(async (req, res) => {
 });
 
 const downloadExpenseExcel = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-  const expense = await Expense.find({ userId }).sort({ date: -1 });
-
-  const data = expense.map((item) => ({
-    Category: item.category,
-    Amount: item.amount,
-    Date: item.date,
-  }));
-
-  const wb = xlsx.utils.book_new();
-  const ws = xlsx.utils.json_to_sheet(data);
-  xlsx.utils.book_append_sheet(wb, ws, "Expense");
-  const buffer = xlsx.write(wb, { bookType: "xlsx", type: "buffer" });
+  const buffer = await expenseService.generateExpenseExcel(req.user._id);
   res.setHeader(
     "Content-Disposition",
     "attachment; filename=expense-details.xlsx",
